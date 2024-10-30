cc9baf328c6a52e6efc8b5c5d0d3bff47ff51cef
GhastRider <omnipotencepara@gmail.com>
2024-10-30 20:09:46 +0800

    :Add new project in turtu

--- hot_150_interview/hot_150_interview.cpp
+++ hot_150_interview/hot_150_interview.cpp
@@ -949,7 +949,7 @@ public:
 
 //28. find-the-index-of-the-first-occurrence-in-a-string
 //https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/
-class Solution_28 //the Legendary KMP
+class Solution_28 //the Legendary KMP -unfinished
 //KMP: 匹配成功的部分中存在相同的【前缀】和【后缀】, 
 //so, 不需要从头匹配，可以将匹配串指针移至【前缀】下一个位置继续匹配
 //=================================================================
@@ -1001,6 +1001,66 @@ public:
 		return -1;
 	}
 };
+
+
+//68. text-justification
+//https://leetcode.cn/problems/text-justification/solutions/181651/text-justification-by-ikaruga/
+
+class Solution_68 //lkaruga题解
+{
+private:
+	string fillWords(vector<string>& words, int bg, int ed, int maxWidth, bool lastLine = false)
+	{
+		int wordCount = ed - bg + 1;
+		int spaceCount = maxWidth - (-1 + wordCount);	
+		
+		//额外空格数量=总长度-正常空格数量-单词长度。
+		
+		for (int i = bg; i <= ed; ++i)
+		{
+			spaceCount -= words[i].size();
+		}
+
+		int spaceSuffix = 1;
+		int spaceAvg = (wordCount == 1) ? 1 : spaceCount/(wordCount-1);
+		int spaceExtra = (wordCount == 1) ? 0 : spaceCount % (wordCount - 1);
+
+		string ans;
+		for (int i = bg; i < ed; ++i) {
+			ans += words[i];
+			if (lastLine)
+			{
+				fill_n(back_inserter(ans), 1, ' ');
+				continue;
+			}
+			fill_n(back_inserter(ans), spaceSuffix + spaceAvg + ((i - bg) < spaceExtra), ' ');
+		}
+		ans += words[ed];
+		fill_n(back_inserter(ans), maxWidth - ans.size(), ' ');
+		return ans;
+	}
+public:
+	vector<string> fullJustify(vector<string>& words, int maxWidth)
+	{
+		vector<string> ans;
+		int cnt = 0;
+		int bg = 0;
+		for (int i = 0; i < words.size(); ++i)
+		{
+			cnt += words[i].size() + 1;
+			if (i + 1 == words.size() || cnt + words[i + 1].size() > maxWidth)
+			{
+				ans.push_back(fillWords(words, bg, i, maxWidth, i + 1 == words.size()));
+				bg = i + 1;
+				cnt = 0;
+				
+			}
+		}
+		return ans;
+	}
+};
+
+
 
 
 
